3c1cccdf408d3a2635236e673111fa17613a88dc
Annie Huang <annie.hn@gmail.com>
2021-08-01 17:08:47 +1000

    Put things into describe block

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-test('should render same text passed into title prop', () => {
-  render(<Header title='My Header' />);
-  const headingElement = screen.getByText(/my header/i); // it's regex so can be small letters
-  expect(headingElement).toBeInTheDocument();
+describe('Header', () => {
+  it('should render same text passed into title prop', () => {
+    render(<Header title='My Header' />);
+    const headingElement = screen.getByText(/my header/i); // it's regex so can be small letters
+    expect(headingElement).toBeInTheDocument();
+  });
 });
 /*
 
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -8,17 +8,19 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => (
   </BrowserRouter>
 );
 
-test('should render the correct amount of incomplete tasks', () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i); // it's regex so can be small letters
-  expect(paragraphElement).toBeInTheDocument();
+describe('TodoFooter', () => {
+  it('should render the correct amount of incomplete tasks', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i); // it's regex so can be small letters
+    expect(paragraphElement).toBeInTheDocument();
+  });
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i); // it's regex so can be small letters
+    expect(paragraphElement).toBeInTheDocument();
+  });
 });
 
-test('should render "task" when the number of incomplete tasks is one', () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i); // it's regex so can be small letters
-  expect(paragraphElement).toBeInTheDocument();
-});
 /*
 
 test('should render "task" when the number of incomplete tasks is one', () => {
